ee04749b202197e8ae4fed791749f6443527e65a
Andrey <plannr@yandex.ru>
2018-12-17 13:56:47 +0700

    logins in jsons

--- server/users.py
+++ server/users.py
@@ -1,9 +1,20 @@
 # dictionary user => password
+import json
 
-PROJECTS_USERS = {
-    'admin': 'admin'
-}
+class Users:
+    def __init__(self, jsonfile):
+        self.jsonfile = jsonfile
 
-DASHBOARD_USERS = {
-    'user': 'user'
-}
+    def __getitem__(self, key):
+        with open(self.jsonfile, 'r') as fd:
+            data = json.load(fd)
+            if not key in data:
+                return ''
+            return data[key]
+
+    def __iter__(self):
+        with open(self.jsonfile, 'r') as fd:
+            return json.load(fd).__iter__()
+
+PROJECTS_USERS = Users('admins.json')
+DASHBOARD_USERS = Users('users.json')
